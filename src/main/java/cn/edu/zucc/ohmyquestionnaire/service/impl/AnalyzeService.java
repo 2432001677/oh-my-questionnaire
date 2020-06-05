4bec460a8f9b5372b96ab4088b03d5feb776ec9f
ybruce <2432001677@qq.com>
2020-06-05 15:46:53 +0800

    分析统计

--- src/main/java/cn/edu/zucc/ohmyquestionnaire/service/impl/AnalyzeService.java
+++ src/main/java/cn/edu/zucc/ohmyquestionnaire/service/impl/AnalyzeService.java
@@ -1,12 +1,9 @@
 package cn.edu.zucc.ohmyquestionnaire.service.impl;
 
-import cn.edu.zucc.ohmyquestionnaire.form.StatisticForm;
-import cn.edu.zucc.ohmyquestionnaire.mongo.bean.BeanAnswers;
 import cn.edu.zucc.ohmyquestionnaire.mongo.repository.AnswersDao;
 import cn.edu.zucc.ohmyquestionnaire.service.IAnalyze;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 public class AnalyzeService implements IAnalyze {
